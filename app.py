4f642aa67691a6ebb11f5adf2791a6c23c046289
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-05-08 19:18:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def main():
             # model = joblib.load('model_lstm_knn_s1_j.pkl')
             # model = load_model('model_lstm_knn_s1.h5')
             # model = keras.models.load_model('model_lstm_knn_s1.keras')
-            model = pickle.load(open('model_lstm_knn_s1.sav', 'rb'))
+            model = pickle.load(open('model_lstm_knn_s1_coco.pkl', 'rb'))
             st.write(model.summary())
             
             # # Memuat data testing (x_test)
@@ -91,81 +91,9 @@ def main():
     #     <h5>Modelling</h5>
     #     <br>
     #     """, unsafe_allow_html=True)
-    #     df5=pd.read_csv('dataset_imputasi_finish_encode_1.csv')
-    #     df_copy = df5.copy()
-    #     df_copy = df_copy[['genre_encode','type_encoder','rating']]
-
-    #     # Memilih kolom yang akan digunakan untuk clustering
-    #     # Gantilah ['fitur_1', 'fitur_2', 'fitur_3'] dengan nama fitur yang sesuai dalam dataset Anda
-    #     X = df_copy[['genre_encode','type_encoder','rating']].values
-
-    #     # Membagi data menjadi data pelatihan dan data pengujian
-    #     X_train, X_test = train_test_split(X, test_size=0.2, random_state=42)
-
-    #     # Membuat objek DBSCAN
-    #     # Sesuaikan nilai epsilon (eps) dan min_samples sesuai kebutuhan Anda
-    #     dbscan = DBSCAN(eps=0.9, min_samples=3)
-
-    #     # Melakukan clustering pada data pelatihan
-    #     labels_train = dbscan.fit_predict(X_train)
-
-    #     # Melakukan clustering pada data pengujian
-    #     labels_test = dbscan.fit_predict(X_test)
-
-    #     # Menambahkan kolom label ke data pelatihan dan pengujian
-    #     data_train = pd.DataFrame(X_train, columns=['genre_encode','type_encoder','rating'])
-    #     data_train['Cluster'] = labels_train
-
-    #     data_test = pd.DataFrame(X_test, columns=['genre_encode','type_encoder','rating'])
-    #     data_test['Cluster'] = labels_test
-
-
-    #     # Menampilkan hasil  cluster dan nilai silhouette score
-    #     col1, col2 =st.columns(2)
-
-    #     with col1:
-    #         st.write("Hasil clustering dengan data pelatihan:")
-    #         st.write(data_train)
-
-    #         score_train = silhouette_score(data_train, data_train['Cluster'])
-    #         st.write("Silhouette score train : {}".format(score_train))
-
-    #     with col2:
-    #         st.write("Hasil clustering dengan data pengujian:")
-    #         st.write(data_test)
-
-    #         score_test= silhouette_score(data_test, data_test['Cluster'])
-    #         st.write("Silhouette score test : {}".format(score_test))
-
+    
     # with tab4:
 
-    #     # Menampilkan hasil clustering pada data pelatihan
-    #     fig_train = plt.figure(figsize=(10, 8))
-    #     ax_train = fig_train.add_subplot(111, projection='3d')
-
-    #     ax_train.scatter(X_train[:, 0], X_train[:, 1], X_train[:, 2], c=labels_train, cmap='viridis', marker='o', s=50)
-
-    #     ax_train.set_title('DBSCAN Clustering - Data Pelatihan')
-    #     ax_train.set_xlabel('Fitur 1')
-    #     ax_train.set_ylabel('Fitur 2')
-    #     ax_train.set_zlabel('Fitur 3')
-
-
-    #     st.pyplot(fig_train)
-
-    #     # Menampilkan hasil clustering pada data pelatihan
-    #     fig_test = plt.figure(figsize=(10, 8))
-    #     ax_test = fig_test.add_subplot(111, projection='3d')
-
-    #     ax_test.scatter(X_test[:, 0], X_test[:, 1], X_test[:, 2], c=labels_test, cmap='viridis', marker='o', s=50)
-
-    #     ax_test.set_title('DBSCAN Clustering - Data Pelatihan')
-    #     ax_test.set_xlabel('Fitur 1')
-    #     ax_test.set_ylabel('Fitur 2')
-    #     ax_test.set_zlabel('Fitur 3')
-
-
-    #     st.pyplot(fig_test)
     
         
 if __name__ == "__main__":
